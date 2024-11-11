db74fdba573cdb439f5af247bcbe1c6bc9b9a335
ardririy <hynnkgw27@gmail.com>
2024-11-11 10:45:35 +0900

    initializeできるようにした

---
+++ cmd/init/init.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"database/sql"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
+)
+
+type Article struct {
+	bun.BaseModel `bun:"table:articles"`
+
+	Name      string    `bun:",pk"` // urlSuffixを持つ(key)
+	FilePath  string    `bun:"file_path,notnull"`
+	Tags      []string  `bun:"type:text[]"`
+	CreatedAt time.Time `bun:"created_at,notnull,default:current_timestamp"`
+	UpdatedAt time.Time `bun:"updated_at,notnull,default:current_timestamp"`
+}
+
+func main() {
+	root, ok := os.LookupEnv("KNOWLEDGES")
+	if !ok {
+		fmt.Println("Environment variable KNOWLEDGES must be set.")
+		os.Exit(1)
+	}
+
+	articles := []Article{}
+
+	dbUsername := os.Getenv("PSQL_USERNAME")
+	password := os.Getenv("PSQL_PASSWORD")
+	dsn := fmt.Sprintf("postgres://%s:%s@localhost:5432/blog?sslmode=disable", dbUsername, password)
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+	db := bun.NewDB(sqldb, pgdialect.New())
+	// SQL文のロギングを有効にする
+	db.AddQueryHook(bundebug.NewQueryHook(
+		bundebug.WithVerbose(true),
+	))
+
+	filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println("Failed to access %s: %v", path, err)
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if filepath.Ext(path) == ".md" {
+			file, err := os.Open(path)
+			if err != nil {
+				fmt.Println("Failed to open %s:", path)
+				fmt.Println(err)
+				return err
+			}
+			scanner := bufio.NewScanner(file)
+			scanner.Scan()
+			firstLine := strings.TrimSpace(scanner.Text())
+			t1, b1 := strings.CutPrefix(firstLine, "<!-- url:")
+			t2, b2 := strings.CutSuffix(t1, "-->")
+			urlSuffix := strings.TrimSpace(t2)
+
+			if b1 && b2 {
+				// 公開対象なのでpathのurlの組をDBに保存
+				// initializeではinsertする。空のDBに対して実行しているので重複しないはず。
+				tags := []string{}
+				scanner.Scan()
+				secontLine := strings.TrimSpace(scanner.Text())
+				fmt.Println(secontLine)
+				t3, b3 := strings.CutPrefix(secontLine, "<!-- tags:")
+				t4, b4 := strings.CutSuffix(t3, "-->")
+				if b3 && b4 {
+					tags = strings.Split(t4, ",")
+				}
+				for i := range tags {
+					tags[i] = strings.TrimSpace(tags[i])
+				}
+				article := Article{
+					Name:     urlSuffix,
+					FilePath: path,
+					Tags:     tags,
+				}
+				articles = append(articles, article)
+			}
+		}
+
+		return nil
+	})
+
+	_, err := db.NewInsert().Model(&articles).Exec(context.Background())
+	if err != nil {
+		fmt.Println("Unknown err: ")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("All Done.")
+
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/bytedance/sonic/loader v0.2.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.6 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.10.0 // indirect
@@ -22,6 +23,7 @@ require (
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -33,6 +35,7 @@ require (
 	github.com/uptrace/bun v1.2.5 // indirect
 	github.com/uptrace/bun/dialect/pgdialect v1.2.5 // indirect
 	github.com/uptrace/bun/driver/pgdriver v1.2.5 // indirect
+	github.com/uptrace/bun/extra/bundebug v1.2.5 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
